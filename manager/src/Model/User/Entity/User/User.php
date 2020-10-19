7c48f1d259c003276fafc27dfdc1b06130efc7b2
mxmilyasov <mxmilyasov@gmail.com>
2020-10-19 23:45:24 +0300

    User entity.

---
+++ manager/src/Model/User/Entity/User/User.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+class User
+{
+    private $email;
+
+    private $passwordHash;
+
+    public function __construct(string $email, string $passwordHash)
+    {
+        $this->email = $email;
+        $this->passwordHash = $passwordHash;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
+
+
+
+}

bf2a95f475506f71ff2484f7da66849ba20502e0
mxmilyasov <mxmilyasov@gmail.com>
2020-10-19 23:46:01 +0300

    Use cases: Command and Handler.

---
+++ manager/src/Model/User/UseCase/SignUp/Request/Command.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\SignUp\Request;
+
+class Command
+{
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+}

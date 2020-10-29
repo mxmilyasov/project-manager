bfb8a7325d1e1d2f9f6284a0f03c153c384eead3
mxmilyasov <mxmilyasov@gmail.com>
2020-10-29 11:42:44 +0300

    Added tests for user roles.

--- manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
+++ manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
+namespace App\Tests\Unit\Model\User\Entity\User\Network;
 
 use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\Network;
